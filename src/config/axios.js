7dba978eccb24c2d7cadeb8dc8e1ba982c609c52
MartinHerreraRamirez <martin.herrera.ramirez.2016@gmail.com>
2023-10-04 17:34:36 -0300

    cambiando endpoint para vercel

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-    baseURL: 'http://localhost:4000/'
+    baseURL: 'https://fake-server-crud-react-redux.vercel.app/productos'
 })
 
 export default clienteAxios
