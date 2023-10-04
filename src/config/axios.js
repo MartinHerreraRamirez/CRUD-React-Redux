654ea3886633ebb063c0998019042d1abcd5eea5
MartinHerreraRamirez <martin.herrera.ramirez.2016@gmail.com>
2023-10-04 17:48:32 -0300

    cambios url

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-    baseURL: 'https://fake-server-crud-react-redux.vercel.app/productos'
+    baseURL: 'https://fake-server-crud-react-redux.vercel.app'
 })
 
 export default clienteAxios
